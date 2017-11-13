80b476edf55cf3b1b5118213e3b5296c593fab17
regbake <regbake@gmail.com>
2017-11-12 18:54:39 -0800

    added signup route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "home" => 'static_pages#home'
   get "help" => 'static_pages#help'
   get "about" =>  'static_pages#about'
+  get "signup" => "users#new"
 
   root "static_pages#home"
 end
