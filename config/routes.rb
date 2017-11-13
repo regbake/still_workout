84fe23e44ae473cfd5b04bfd71c12d545058373c
regbake <regbake@gmail.com>
2017-11-12 19:58:53 -0800

    sign up is buggy

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   get 'users/new'
 
-  # get "home" => 'static_pages#home'
-  #
-  # get "help" => 'static_pages#help'
-
   get "home" => 'static_pages#home'
   get "help" => 'static_pages#help'
   get "about" =>  'static_pages#about'
   get "signup" => "users#new"
+  post "signup" => "users#create"
 
   root "static_pages#home"
+
+  resources :users
 end
