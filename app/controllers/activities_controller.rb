bcf009956cd8358a1dbb73328b402fc5b7262a89
regbake <regbake@gmail.com>
2017-11-13 15:06:58 -0800

    delete works on the activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,4 +1,7 @@
 class ActivitiesController < ApplicationController
+  # before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
+
   def create
     @activity = current_user.activities.build(activity_params)
     if @activity.save
@@ -13,11 +16,19 @@ class ActivitiesController < ApplicationController
   end
 
   def destroy
+    @activity.destroy
+    flash[:success] = "Activity deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def activity_params
       params.require(:activity).permit(:comments, :date, :activity, :minutes)
+    end
+
+    def correct_user
+      @activity = current_user.activities.find_by(id: params[:id])
+      redirect_to root_url if @activity.nil?
     end
 end
