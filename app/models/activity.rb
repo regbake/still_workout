96941b73f966f1497b3e6af3c93348eff171b642
regbake <regbake@gmail.com>
2017-11-14 14:19:44 -0800

    getting ready to make multipart form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,9 @@
 class Activity < ApplicationRecord
   belongs_to :user
 
+  validates :date, presence: true
+  validates :activity, presence: true
+  validates :minutes, presence: true
   # default_scope -> { order(date: :desc) }
   validates :user_id, presence: true
 end
