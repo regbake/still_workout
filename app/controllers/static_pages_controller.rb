af2f88e6237da13e5fe4e352cb5bab4ce194f32c
regbake <regbake@gmail.com>
2017-11-15 17:12:15 -0800

    graphs galore

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @activity = current_user.activities.build if logged_in?
+    @show = current_user.activities.all
   end
 
   def help
