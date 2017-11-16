59c420d9e6bc184f7b995f93e2f989d62c982d4a
regbake <regbake@gmail.com>
2017-11-16 10:40:49 -0800

    working on styling, centering into a one column format

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @activity = current_user.activities.build if logged_in?
-    @show = current_user.activities.all
+    @show = current_user.activities.all if logged_in?
   end
 
   def help
